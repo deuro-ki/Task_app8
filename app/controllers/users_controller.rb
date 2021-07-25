11cc86da34716cdbebb76df124163321d9539396
“deuro” <hero14yki1986@gmail.com>
2021-07-25 21:04:25 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = '新規作成に成功しました。'
+      log_in @user
       redirect_to @user
     else
       render :new
